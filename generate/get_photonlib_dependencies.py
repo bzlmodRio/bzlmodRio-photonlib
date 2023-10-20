8462ec4476614d1e9ad534414ceeea8629c22149
PJ Reiniger <pj.reiniger@gmail.com>
2023-10-20 18:36:44 -0400

    Initial generation

---
+++ generate/get_photonlib_dependencies.py
@@ -0,0 +1,62 @@
+import os
+
+from bazelrio_gentool.load_vendordep_dependency import vendordep_dependency
+from get_allwpilib_dependencies import get_allwpilib_dependencies
+from bazelrio_gentool.deps.dependency_container import (
+    ModuleDependency,
+)
+
+
+def get_photonlib_dependencies(
+    use_local_allwpilib=False, use_local_opencv=False, use_local_ni=True,
+    allwpilib_version_override="2023.3.2",
+    opencv_version_override="4.6.0-4",
+    ni_version_override="2023.3.0",
+):
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    group = vendordep_dependency(
+        "bzlmodrio-photonlib",
+        os.path.join(SCRIPT_DIR, f"vendor_dep.json"),
+        year=2023,
+        fail_on_hash_miss=False,
+        has_static_libraries=False,
+    )
+
+    allwpilib_dependency = ModuleDependency(
+        get_allwpilib_dependencies(
+            use_local_opencv=use_local_opencv, use_local_ni=use_local_ni,
+            opencv_version_override=opencv_version_override,
+            ni_version_override=ni_version_override,
+        ),
+        use_local_version=use_local_allwpilib,
+        local_rel_folder="../../libraries/bzlmodRio-allwpilib",
+        remote_repo="bzlmodRio-allwpilib",
+        override_version=allwpilib_version_override,
+    )
+    group.add_module_dependency(allwpilib_dependency)
+
+    
+    group.add_cc_meta_dependency(
+        "photonlib-cpp",
+        deps=[
+            "PhotonLib-cpp",
+            "wpilibc-cpp",
+        ],
+        platform_deps={},
+        jni_deps={
+            # TODO
+        },
+    )
+    
+    
+    
+    group.add_java_meta_dependency(
+        "photonlib-java",
+        group_id=f"org.photonvision",
+        deps=[
+            "wpilibj-java",
+        ],
+    )
+    
+    
+    return group
---
+++ generate/publish.py
@@ -0,0 +1,21 @@
+import os
+from get_photonlib_dependencies import get_photonlib_dependencies
+from bazelrio_gentool.generate_json import generate_json
+
+
+def main():
+    SCRIPT_DIR = os.environ["BUILD_WORKSPACE_DIRECTORY"]
+    registry_location = os.path.join(
+        SCRIPT_DIR, "..", "..", "..", "bazel-central-registry"
+    )
+
+    group = get_photonlib_dependencies()
+
+    os.chdir(SCRIPT_DIR)
+    generate_json(
+        registry_location, group, module_json_template=None, module_template=None
+    )
+
+
+if __name__ == "__main__":
+    main()
